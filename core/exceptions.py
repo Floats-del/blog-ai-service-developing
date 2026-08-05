051c62d8531fb37d3249b7a14771e3da49a2cad3
Floats-del <inboxmohib@gmail.com>
2026-08-05 22:47:55 +0500

    fixed imports, even tho proj is done dw

--- core/exceptions.py
+++ core/exceptions.py
@@ -3,68 +3,12 @@ from utils.logging.logEvents import ExceptionLog
 
 class AppException(Exception):
     """Base exception for application-level errors."""
-    # Default fallback log event for any generic app exception
     log_event: ExceptionLog = ExceptionLog.APP_EXCEPTION
 
     def __init__(self, error_code: str, message: str):
         self.error_code = error_code
         self.message = message
         super().__init__(message)
-
-#why we call super on message?
-"""
-know that our class inharits Exception class
-normal use:
-try:
-    raise Exception("Database failed")
-
-except Exception as e:
-    print(e)
-
-output: Database failed
-
-
-
-rn our class has error_code and message member var we can:
-exc.error_code
-exc.message
-
-to get answers BUT
-like in normal use when we raise we do:
-raise Exception(str)
-
-but in this calls we would do:
-
-raise AppException(
-    "AI_ERROR",
-    "Model failed"
-)
-
-but rn Exception doesnt know what data was passed in child's class?
-
-
-super().__init__(message) means:
-"Parent Exception, initialize yourself using this message."
-
-coz Exception class intenrally also has member var message we use that one here!
-so that member var has all the attributes Exception class had! so in child they're
-inharited and now when we pass a message var! and print it we get output
-as normal use's example!
-
-
-to show u how messge var which we inharited would act:
-try:
-    raise AppException(
-        "AI_ERROR",
-        "Model failed"
-    )
-
-except Exception as e:
-    print(e)
-    
-output: Model failed -> the other didnt came coz it was our custiom we gotta call that ourselvs lol
-"""
-
 
 
 
@@ -73,7 +17,7 @@ class AIServiceException(AppException):
     """
     AI related failures.
     """
-    # Override the log event specifically for this subclass! (so for each class we have respic )
+    
     log_event: ExceptionLog = ExceptionLog.AI_SERVICE_EXCEPTION
 
 
@@ -88,12 +32,56 @@ class PostServiceException(AppException):
 
 
 
+class ChangePasswordException(AppException):
+    """
+    if somehow password wasnt changed
+    """
+    
+    log_event: ExceptionLog = ExceptionLog.PASSWORD_CHANGE_EXCEPTION
+
+
 class LoginServiceException(AppException):
     """
     Login and jwt creation faluires
     """
     
     log_event: ExceptionLog = ExceptionLog.LOGIN_EXCEPTION
+
+
+
+class LogoutServiceException(AppException):
+    """
+    Logout issue, if someone logsout when they're logged out
+    """
+    
+    log_event: ExceptionLog = ExceptionLog.LOGOUT_EXCEPTION
+
+
+
+class LogoutAllDeviServiceException(AppException):
+    """
+    Logout form all devices issue
+    """
+    
+    log_event: ExceptionLog = ExceptionLog.LOGOUT_ALL_DEVICES_EXCEPTION
+
+
+
+class AllServiceContException(AppException):
+    """
+    when user wanna know how many active sessins -> perhspas in app settings this feature will be good
+    """
+    
+    log_event: ExceptionLog = ExceptionLog.ACTIVE_SESSION_COUNT_EXCEPTION
+
+
+
+class RvokeCurrentSessionException(AppException):
+    """
+    use when user is hacked and he appils, revoke his 1 device which is hacked's session, rest device no, keep em
+    """
+    
+    log_event: ExceptionLog = ExceptionLog.REVOKATION_CURRENT_SESSION_EXCEPTION
 
 
 
@@ -110,4 +98,3 @@ class LikeServiceException(AppException):
 class UserCreationServiceException(AppException):
     log_event: ExceptionLog = ExceptionLog.USER_CREATION_EXCEPTION
     
-#we can keep adding other classes for diff uses for since each will inahirt AppException which Inharits Excepion so dw
